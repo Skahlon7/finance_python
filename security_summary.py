fa57cad838ff3ee184d81f2a9fd2b831df79eb6f
Sandeep Kahlon <86175937+Skahlon7@users.noreply.github.com>
2021-09-03 23:06:29 -0700

    Security Transaction Summary
    Profit/loss + transaction summary with set commission cost derived from Robinhood and ETADE brokerages.

---
+++ security_summary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+stock_p = float(input("Stock Purchace Price: "))
+stock_s = float(input("Stock Selling Price: "))
+shares = int(input("Shares: "))
+print(f'Purchace amount: ${shares*stock_p:.2f}')
+print(f'Selling amount:  ${shares*stock_s:.2f}')
+print(f'Comission cost:  ${(shares*stock_s)*.015:.2f}')
+print(f'Profit/Loss:     ${(shares*stock_s) - (shares*stock_p) - ((shares*stock_s)*.03):.2f}')
+
